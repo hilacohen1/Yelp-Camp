c3437954aa077aa84fc7570a823a12d93b178312
hilacohen1 <hila9965@gmail.com>
2020-11-11 15:49:40 +0200

    connect to database

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground= require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("mongo connection open");
+    })
+    .catch(err => {
+        console.log("oh no error");
+        console.log(err);
+    })
+
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req,res)=>{
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
     res.send('Home Page');
 })
+
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+})
+
 
 app.listen(3000, () => {
     console.log("servint on port 3000");
