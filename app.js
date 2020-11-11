d10f6e27d46049f2a9215de4a211da3bb1ecf96e
hilacohen1 <hila9965@gmail.com>
2020-11-11 17:01:59 +0200

    add new campground option

--- app.js
+++ app.js
@@ -29,6 +29,20 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/new', async(req,res)=>{
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req,res)=>{
+    const campground=new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+})
+
+app.get('/campgrounds/:id', async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{ campground });
+})
 
 app.listen(3000, () => {
     console.log("servint on port 3000");
