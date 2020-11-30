cc4049bce36ba4f6e6b8776cc0e6d5059de2b0c1
hilacohen1 <hila9965@gmail.com>
2020-11-30 12:21:50 +0200

    Adding Authentication

--- app.js
+++ app.js
@@ -6,24 +6,33 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(() => {
-        console.log("mongo connection open");
-    })
-    .catch(err => {
-        console.log("oh no error");
-        console.log(err);
-    })
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { 
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true, 
+    useFindAndModify: false 
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection error:"));
+db.once("open", ()=>{
+    console.log("Database connected");
+});
 
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
@@ -42,14 +51,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.send('Home Page');
