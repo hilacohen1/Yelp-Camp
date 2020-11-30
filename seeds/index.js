ce59a329cb0684594aefd997fc7f2ab9ef887399
hilacohen1 <hila9965@gmail.com>
2020-11-30 18:02:46 +0200

    adding authorization

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author:'5fc4c4792210fc019cdaad26',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
